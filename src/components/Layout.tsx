40a31827f6eec35b18bfc53368ceab0af8b4b087
RedPeanut <baekwon10000@gmail.com>
2021-08-01 21:03:46 +0900

    스타일 전반적으로 만짐

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,20 +1,25 @@
-import React, { useState } from "react"
+import React, { ReactNode } from "react"
 import { Helmet } from 'react-helmet'
 import "../../static/css/style.css"
 import Icon from "./Icon"
 import ICONS from "../utils/icons"
 import { Link } from "gatsby"
 
-export default function Layout({ children }) {
+interface LayoutProps {
+  className: string
+  children?: ReactNode
+}
+
+export default function Layout({ className, children }) {
   return (
-    <>
+    <div className={className}>
       <Helmet>
         <title>김진규블로그</title>
         {/* <link rel="stylesheet" href={style} /> */}
       </Helmet>
       {/* <Styled.GlobalStyle /> */}
       { children }
-    </>
+    </div>
   )
 }
 
@@ -28,7 +33,7 @@ export function PlainLayout({ children }) {
 
 interface HomeLayoutProps {
   index: number
-  children: any
+  children?: ReactNode
 }
 
 export function HomeLayout({ index = 0, children }: HomeLayoutProps) {
@@ -37,39 +42,44 @@ export function HomeLayout({ index = 0, children }: HomeLayoutProps) {
   console.log("index = " + index)
 
   return (
-    <Layout>
-      <header>
-        <h1>
-          <a className="" href="/">김진규블로그</a>
-        </h1>
-        <nav>
-          <ul>
-            <li><Link to="/about" className={index == 0 ? 'active' : ''}>About</Link></li>
-            <li><Link to="/blog" className={index == 1 ? 'active' : ''}>Blog</Link></li>
-            <li><Link to="/gallery" className={index == 2 ? 'active' : ''}>Gallery</Link></li>
-            {/* <li><a href="/about" className={index == 0 ? 'active' : ''}>About</a></li>
-            <li><a href="/blog" className={index == 1 ? 'active' : ''}>Blog</a></li>
-            <li><a href="/gallery" className={index == 2 ? 'active' : ''}>Gallery</a></li> */}
-          </ul>
-        </nav>
-        <ul className="contacts">
-          <li>
-            <a href="mailto:zamong99@gmail.com" target="_blank">
-              <Icon name="email" icon={ICONS['EMAIL']}/>
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/RedPeanut/RedPeanut.github.io.src" target="_blank">
-              <Icon name="github" icon={ICONS['GITHUB']}/>
-            </a>
-          </li>
-        </ul>
-      </header>
-      { children }
-      <footer>
-        © 2021 by 김진규. All right reserved.
-      </footer>
+    <Layout className="home layout">
+        <header>
+          <div className="wrap">
+            <h1>
+              <a className="" href="/">김진규블로그</a>
+            </h1>
+            <nav>
+              <ul>
+                <li><Link to="/about" className={index == 0 ? 'active' : ''}>About</Link></li>
+                <li><Link to="/blog" className={index == 1 ? 'active' : ''}>Blog</Link></li>
+                <li><Link to="/gallery" className={index == 2 ? 'active' : ''}>Gallery</Link></li>
+                {/* <li><a href="/about" className={index == 0 ? 'active' : ''}>About</a></li>
+                <li><a href="/blog" className={index == 1 ? 'active' : ''}>Blog</a></li>
+                <li><a href="/gallery" className={index == 2 ? 'active' : ''}>Gallery</a></li> */}
+              </ul>
+            </nav>
+            <ul className="contacts">
+              <li>
+                <a href="mailto:zamong99@gmail.com" target="_blank">
+                  <Icon name="email" icon={ICONS['EMAIL']}/>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/RedPeanut/RedPeanut.github.io.src" target="_blank">
+                  <Icon name="github" icon={ICONS['GITHUB']}/>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </header>
+        { children }
+        <footer>
+          <div className="wrap">
+            <p>© 2021 by 김진규. All right reserved.</p>
+          </div>
+        </footer>
     </Layout>
   )
 }
+
 
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,40 +4,62 @@ import { HomeLayout } from "../components/Layout"
 export default function index() {
   return (
     <HomeLayout index={0}>
-      <section className="career">
-        <div>
-          <h1>경력</h1>
-          <div className="item">
-          <b>디큐</b><span className="period">2017.02. - 현재</span>
-          <span className="position">웹개발</span>
+      <div className="about">
+        <section className="career">
+          <div className="wrap">
+            <div>
+              <h1>경력</h1>
+              <div className="item">
+              <b>디큐</b><span className="period">2017.02. - 현재</span>
+              <span className="position">웹개발(과장)</span>
+              </div>
+              <div className="item">
+              <b>필아이티</b><span className="period">2013.02. - 2017.02.</span>
+              <span className="position">앱개발(선임연구원)</span>
+              </div>
+              <div className="item">
+              <b>인스모바일</b><span className="period">2008.12. - 2012.12.</span>
+              <span className="position">앱개발(주임연구원)</span>
+              </div>
+              <div className="item">
+              <b>마루인포</b><span className="period">2008.03. - 2008.10.</span>
+              <span className="position">앱개발(사원)</span>
+              </div>
+            </div>
+            <div>
+              <h1>학력</h1>
+              <div className="item">
+              <b>건국대학교</b><span className="period">1999. - 2006.</span>
+              <span className="position">전기공학과 학사</span>
+              </div>
+            </div>
           </div>
-          <div className="item">
-          <b>필아이티</b><span className="period">2013.02. - 2017.02.</span>
-          <span className="position">앱개발</span>
+        </section>
+        <section className="skill">
+          <div className="wrap">
+            <h1>보유기술</h1>
+            <div className="line">
+              Android, Java, Spring, myBatis, Eclipse
+            </div>
           </div>
-          <div className="item">
-          <b>인스모바일</b><span className="period">2008.12. - 2012.12.</span>
-          <span className="position">앱개발</span>
+        </section>
+        <section className="skill">
+          <div className="wrap">
+            <h1>그외</h1>
+            <div className="line">
+              Svn, Git, DB 마이그레이션, Linux, Apache, Tomcat, C, HTML, CSS, Javascript
+            </div>
           </div>
-          <div className="item">
-          <b>마루인포</b><span className="period">2008.03. - 2008.10.</span>
-          <span className="position">앱개발</span>
+        </section>
+        <section className="interest">
+          <div className="wrap">
+            <h1>관심분야</h1>
+            <div className="line">
+              Node
+            </div>
           </div>
-        </div>
-        <div>
-          <h1>학력</h1>
-          <div className="item">
-          <b>건국대학교</b><span className="period">1999. - 2006.</span>
-          <span className="position">전기공학과 학사</span>
-          </div>
-        </div>
-      </section>
-      <section className="interest">
-        <h1>관심분야</h1>
-        <div className="line">
-          Keyword, Keyword, Keyword, Keyword, Keyword
-        </div>
-      </section>
+        </section>
+      </div>
     </HomeLayout>
   )
 }
